faf334f1e29aa202b2e6a7657f55572755086fb6
Thamoddya Rashmitha <thamoddyarashmithadissanayake@gmail.com>
2025-04-20 10:31:05 +0530

    Add initial project structure with essential files and logging setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="Medical Diagnosis Chatbot",
+    version="0.0.1",
+    author="Thamoddya Rashmitha",
+    author_email="thamoddyarashmithadissanayake@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
